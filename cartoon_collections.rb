bc399194004c29bc88904191c4adcd3c9b89d9c2
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-01 19:33:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,9 @@ end
 
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
+  
+end
+
+# Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-end
