d26b92779cbcfe6caef537bebe9a5ede3dc02866
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-01 20:22:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,9 @@ end
 
 def list_dwarves(array)
   
-    array.each_with_index do |dwarves, index|
+    array.each_with_index do |name, index|
     
-      puts #{index +1}. dwarves
+      puts #{index +1}. name
     
     end  
 end
