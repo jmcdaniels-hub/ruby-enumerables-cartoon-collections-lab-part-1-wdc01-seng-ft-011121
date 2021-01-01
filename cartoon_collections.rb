a9103afddccbd030a12e029c018e7384b4b9a17e
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-01 19:30:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,15 @@
 def greet_characters(array)
+  
+  
+  
+  
+  
+end  
+  
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
+
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
